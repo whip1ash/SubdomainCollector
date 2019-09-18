2f03090526c8652511b04d44b1453788640f1381
Yoga7xm <yujianan1998@qq.com>
2019-09-18 15:42:51 +0800

    目录结构

--- lijiejie/lijiejie_options.py
+++
@@ -1,8 +0,0 @@
-class lijiejie_options:
-    def __init__(self):
-        self.debug = True
-        self.file = 'subnames.txt'
-        self.full_scan = False
-        self.i = False
-        self.output = None
-        self.threads = 5
